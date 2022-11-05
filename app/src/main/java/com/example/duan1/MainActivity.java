2b7288320e000781cda0b98b97eb49dee34b740f
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-05 22:28:46 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.duan1;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("CC", "CC");
-        Toast.makeText(this, "vc", Toast.LENGTH_SHORT).show();
     }
 }
