a1e3ff8e7720e7abb2e0b15b15c4c5f0ffc352ec
TrongDat296 <115761014+TrongDat296@users.noreply.github.com>
2022-11-08 07:35:51 +0700

    Hello

---
+++ app/src/main/java/com/example/duan1/views/CheckOutActivity.java
@@ -0,0 +1,28 @@
+package com.example.duan1.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.duan1.R;
+
+public class CheckOutActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+        anhXa();
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Check Out");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void anhXa() {
+        toolbar = findViewById(R.id.toolbar);
+    }
+}
--- app/src/main/java/com/example/duan1/views/MainActivity.java
+++ app/src/main/java/com/example/duan1/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.duan1;
+package com.example.duan1.views;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.duan1.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD:app/src/main/java/com/example/duan1/MainActivity.java
         //truondinththaotest
         // sdffhdsafsdafdsfdsfsdjfdjkfd
         // Huynh Hong Vy test
         // thao gei
+        //tromng=dat
+        //ajlkskdkalksjdlkajslkdjlakjskdjlksd
+=======
+>>>>>>> aec8208915242a46adccf04134c1cd11eef2b357:app/src/main/java/com/example/duan1/views/MainActivity.java
     }
 }
