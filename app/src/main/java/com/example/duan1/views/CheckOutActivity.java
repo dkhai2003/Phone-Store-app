9ed51fad61413ac6082b5ce33ee2cc21bb6b881c
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-08 09:05:13 +0700

    CheckOut
    Update

--- app/src/main/java/com/example/duan1/views/CheckOutActivity.java
+++ app/src/main/java/com/example/duan1/views/CheckOutActivity.java
@@ -1,15 +1,19 @@
 package com.example.duan1.views;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.duan1.R;
+import com.example.duan1.fragment.BottomSheetDialog;
 
 public class CheckOutActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private Button btnConfirmAndPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,17 @@ public class CheckOutActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Check Out");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btnConfirmAndPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = BottomSheetDialog.newInstance();
+                bottomSheetDialog.show(getSupportFragmentManager(), bottomSheetDialog.getTag());
+            }
+        });
     }
 
     private void anhXa() {
         toolbar = findViewById(R.id.toolbar);
+        btnConfirmAndPay = findViewById(R.id.btnConfirmAndPay);
     }
 }
