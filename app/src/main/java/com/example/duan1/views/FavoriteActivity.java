52fc21233ae9119c2ca2e88f126c7cd891fa9569
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-10 08:02:49 +0700

    Update

--- app/src/main/java/com/example/duan1/views/FavoriteActivity.java
+++ app/src/main/java/com/example/duan1/views/FavoriteActivity.java
@@ -1,16 +1,39 @@
 package com.example.duan1.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.duan1.R;
 
 public class FavoriteActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Button btnStartOrdering;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        anhXa();
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Favorites");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btnStartOrdering.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FavoriteActivity.this, "This is Button Start Ordering", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void anhXa() {
+        toolbar = findViewById(R.id.toolbar);
+        btnStartOrdering = findViewById(R.id.btnStartOrdering);
     }
 }
