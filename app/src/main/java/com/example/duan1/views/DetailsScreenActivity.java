2ab7b85e925543e67a7275bb546e4713661bbb76
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-10 09:40:08 +0700

    Update Thao

---
+++ app/src/main/java/com/example/duan1/views/DetailsScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.duan1.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.duan1.R;
+
+
+public class DetailsScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_screen);
+    }
+}
---
+++ app/src/main/java/com/example/duan1/views/UserActivity.java
@@ -0,0 +1,16 @@
+package com.example.duan1.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.duan1.R;
+
+
+public class UserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
