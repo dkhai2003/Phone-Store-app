793314ea6b63752b0256fe55e6d187aca6b3eaa1
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-11 22:19:34 +0700

    Revert "Revert "LoginDat""
    This reverts commit 3e853cfabdb7cd5a1a0095f701299267feae17a9.

--- app/src/main/java/com/example/duan1/views/LoginActivity.java
+++ app/src/main/java/com/example/duan1/views/LoginActivity.java
@@ -2,15 +2,45 @@ package com.example.duan1.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.MailTo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.duan1.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView tvHello,tvLogin,tvForgot,tvCreate;
+    Button btnLogin;
+    LinearLayout lnEmail,lnPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        anhXa();
+
+
+    }
+
+    public void setTvCreate(View view){
+        Intent intent= new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+    public void setBtnLogin(View view){
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void anhXa(){
+        tvHello=(TextView) findViewById(R.id.tvHello);
+        tvLogin=(TextView) findViewById(R.id.tvLogin);
+        tvForgot=(TextView) findViewById(R.id.tvForgot);
+        tvCreate=(TextView) findViewById(R.id.tvCreate);
+        btnLogin=(Button) findViewById(R.id.btnLogin);
+        lnEmail=(LinearLayout) findViewById(R.id.lnEmail);
+        lnPass=(LinearLayout) findViewById(R.id.lnPass);
     }
 }
