3e853cfabdb7cd5a1a0095f701299267feae17a9
TrongDat296 <115761014+TrongDat296@users.noreply.github.com>
2022-11-11 22:14:46 +0700

    Revert "LoginDat"
    This reverts commit e9aa5b7b6374d4a86c3de33edce7546ae06d530b.

--- app/src/main/java/com/example/duan1/views/HelloActivity.java
+++ app/src/main/java/com/example/duan1/views/HelloActivity.java
@@ -8,7 +8,6 @@ import com.example.duan1.R;
 
 public class HelloActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
