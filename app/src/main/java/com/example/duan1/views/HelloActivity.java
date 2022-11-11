c8e04b5be5ba3828ffc4492ea9bdb6035806a3f2
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-11 23:31:25 +0700

    Update HelloActivity.java

--- app/src/main/java/com/example/duan1/views/HelloActivity.java
+++ app/src/main/java/com/example/duan1/views/HelloActivity.java
@@ -13,6 +13,6 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
-        //adu
+        //vy
     }
 }
