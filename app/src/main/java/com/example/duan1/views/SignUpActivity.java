1c2e27175a086e0e2fe15d539140ce9ff210b9a3
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-15 09:11:29 +0700

    Update

--- app/src/main/java/com/example/duan1/views/SignUpActivity.java
+++ app/src/main/java/com/example/duan1/views/SignUpActivity.java
@@ -2,8 +2,6 @@ package com.example.duan1.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.duan1.R;
 import com.example.duan1.model.User;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -85,37 +82,14 @@ public class SignUpActivity extends AppCompatActivity {
         edtEmail = (EditText) findViewById(R.id.edtEmail);
         edtPassword = (EditText) findViewById(R.id.edtPassword);
         edtConfirm_pass = (EditText) findViewById(R.id.edtConfirm_pass);
-        TextInputLayout input1 = findViewById(R.id.input1);
-        input1.getEditText().addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-//                if (pass.isEmpty()) {
-//                    edtPassword.setError("Password is empty !");
-//                }
-//                if (rePass.isEmpty()) {
-//                    edtConfirm_pass.setError("Re Password is empty !");
-//                }
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.length() == 0) {
-                    input1.setError("Email is empty !");
-                    input1.setErrorEnabled(true);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
     }
 
     public Boolean checkValidate(String tenDangNhap, String pass, String rePass) {
-        if (!tenDangNhap.isEmpty() || !pass.isEmpty() || !rePass.isEmpty()) {
+        if (!tenDangNhap.isEmpty() && !pass.isEmpty() && !rePass.isEmpty()) {
             return true;
+        }
+        if (tenDangNhap.isEmpty()) {
+            edtEmail.setError("Email is empty !");
         }
         return false;
     }
