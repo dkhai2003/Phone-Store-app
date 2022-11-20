4be11d9a560284ce8ff542267788a51155a20e64
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-20 16:13:08 +0700

    a

--- app/src/main/java/com/example/duan1/fragment/HomeFragment.java
+++ app/src/main/java/com/example/duan1/fragment/HomeFragment.java
@@ -86,21 +86,7 @@ public class HomeFragment extends Fragment {
 
     }
 
-    //    public void setUserInformation() {
-//    private void txtSreach(String str){
-//            FirebaseRecyclerOptions<Product> options =
-//                    new FirebaseRecyclerOptions.Builder<Product>()
-//                            .setQuery(FirebaseDatabase.getInstance().getReference().child("duan").child("LoaiSanPham").child("lsp1").child("SanPham").orderByChild("tenSP").startAt(str).endAt(str+"~"), Product.class)
-//                            .build();
-//
-//            productAdapter = new ProductAdapter(options);
-//            productAdapter.startListening();
-//            recyclerViewListProduct.setAdapter(productAdapter);
-//        }
-//    }
-
-//}
-
+    
     private void txtSreach(String str) {
         FirebaseRecyclerOptions<Product> options =
                 new FirebaseRecyclerOptions.Builder<Product>()
