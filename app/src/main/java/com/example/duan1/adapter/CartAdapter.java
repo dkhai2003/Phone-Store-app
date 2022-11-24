f166f334be060c8c764f961cbcb3245d6c3a8615
mujga <99776573+mujga@users.noreply.github.com>
2022-11-24 08:27:09 +0700

    update tvItems

--- app/src/main/java/com/example/duan1/adapter/CartAdapter.java
+++ app/src/main/java/com/example/duan1/adapter/CartAdapter.java
@@ -1,5 +1,8 @@
 package com.example.duan1.adapter;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,9 @@ import com.example.duan1.R;
 import com.example.duan1.model.Product;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CartAdapter extends FirebaseRecyclerAdapter<Product,CartAdapter.myViewHolder> {
 
@@ -37,10 +43,35 @@ public class CartAdapter extends FirebaseRecyclerAdapter<Product,CartAdapter.myV
                 .load(model.getHinhSP())
                 .into(holder.img);
 
-        holder.cardViewCart.setOnClickListener(new View.OnClickListener() {
+//        holder.cardViewCart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                iClickCart.onClickDeleteCart(model);
+//            }
+//        });
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 iClickCart.onClickDeleteCart(model);
+//                AlertDialog.Builder alerBuider = new AlertDialog.Builder(v.getContext());
+//                alerBuider.setTitle("Notifications");
+//                alerBuider.setMessage("Do you want to delete?");
+//                alerBuider.setCancelable(false);
+//                alerBuider.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//                        String userEmail = user.getEmail();
+//                        String[] subEmail = userEmail.split("@");
+//                        String pathUserId = "User" + subEmail[0];
+//                        FirebaseDatabase.getInstance().getReference("duan/User").child(pathUserId).child("Cart").child(model.getMaSP()).removeValue();
+//                    }
+//                }).setNegativeButton("no",null);
+//                Dialog dialog = alerBuider.create();
+//                dialog.show();
+
+
             }
         });
 
@@ -54,7 +85,7 @@ public class CartAdapter extends FirebaseRecyclerAdapter<Product,CartAdapter.myV
     }
 
     static class myViewHolder extends RecyclerView.ViewHolder {
-        ImageView img;
+        ImageView img, imgDelete;
         TextView name, price;
         CardView cardViewCart;
 
@@ -67,6 +98,7 @@ public class CartAdapter extends FirebaseRecyclerAdapter<Product,CartAdapter.myV
             name = itemView.findViewById(R.id.tvNameCart);
             price = itemView.findViewById(R.id.tvPriceCart);
             cardViewCart = itemView.findViewById(R.id.card_view_cart);
+            imgDelete = itemView.findViewById(R.id.imgDeleteCart);
 
         }
     }
--- app/src/main/java/com/example/duan1/fragment/CartFragment.java
+++ app/src/main/java/com/example/duan1/fragment/CartFragment.java
@@ -1,12 +1,16 @@
 package com.example.duan1.fragment;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.viewmodel.CreationExtras;
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -17,20 +21,24 @@ import com.example.duan1.R;
 import com.example.duan1.adapter.CartAdapter;
 import com.example.duan1.model.Product;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class CartFragment extends Fragment  {
 
     public static final String TAG = CartFragment.class.getName();
-
+    private TextView tvCountCart;
     private View mView;
     private RecyclerView recyclerViewCart;
     private CartAdapter cartAdapter;
-
+    int mcount_cart = 0;
 
 
     public static CartFragment newInstance() {
@@ -59,6 +67,7 @@ public class CartFragment extends Fragment  {
 
     private void uniUi(){
         recyclerViewCart = mView.findViewById(R.id.recyclerviewListCart);
+         tvCountCart = mView.findViewById(R.id.tvCountCart);
     }
 
     public String getUserID(String pathUserId){
@@ -106,11 +115,61 @@ public class CartFragment extends Fragment  {
         cartAdapter = new CartAdapter(options, new CartAdapter.IClickCart() {
             @Override
             public void onClickDeleteCart(Product product) {
-                FirebaseDatabase.getInstance().getReference("duan/User").child(pathUserId).child("SanPham").child(product.getMaSP()).removeValue();
+//                FirebaseDatabase.getInstance().getReference("duan/User").child(pathUserId).child("SanPham").child(product.getMaSP()).removeValue();
+                AlertDialog.Builder alerBuider = new AlertDialog.Builder(getContext());
+                alerBuider.setTitle("Notifications");
+                alerBuider.setMessage("Do you want to delete?");
+                alerBuider.setCancelable(false);
+                alerBuider.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        String userEmail = user.getEmail();
+                        String[] subEmail = userEmail.split("@");
+                        String pathUserId = "User" + subEmail[0];
+                        FirebaseDatabase.getInstance().getReference("duan/User").child(pathUserId).child("Cart").child(product.getMaSP()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getContext(), "Remove Success", Toast.LENGTH_SHORT).show();
+                                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        mcount_cart = (int) snapshot.getChildrenCount();
+                                        tvCountCart.setText(mcount_cart+" items");
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+
+                            }
+                        });
+                    }
+                }).setNegativeButton("no",null);
+                Dialog dialog = alerBuider.create();
+                dialog.show();
+                
             }
         });
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mcount_cart = (int) snapshot.getChildrenCount();
+                tvCountCart.setText(mcount_cart+" items");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        
+        
 
         recyclerViewCart.setAdapter(cartAdapter);
+        cartAdapter.notifyDataSetChanged();
 //        itemTouchHelper.attachToRecyclerView(recyclerViewCart);
 
 
