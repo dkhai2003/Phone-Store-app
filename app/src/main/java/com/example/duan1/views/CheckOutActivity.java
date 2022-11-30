4b2437eebbf3b2006ba5f3d65020573a8e102d57
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-11-30 22:46:14 +0700

    Update CheckOutActivity.java

--- app/src/main/java/com/example/duan1/views/CheckOutActivity.java
+++ app/src/main/java/com/example/duan1/views/CheckOutActivity.java
@@ -57,17 +57,9 @@ public class CheckOutActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(CheckOutActivity.this, "This is Button PayNow", Toast.LENGTH_SHORT).show();
-
-<<<<<<< HEAD
+                
                 finish();
                 bottomSheetDialog.dismiss();
-=======
-               // updateBillToFireBase();
-                updateBillToFireBase();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(androidx.appcompat.R.id.home, new HomeFragment()).commit();
-
->>>>>>> parent of 042686d (Merge branch 'test02' into Vy)
             }
         });
         bottomSheetDialog.setContentView(viewDialog);
