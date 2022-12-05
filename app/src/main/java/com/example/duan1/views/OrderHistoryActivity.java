5e5c7d4024d957f64240442843e1d43a730ebfde
Huỳnh Hồng Vỹ <93599524+nevergiveup2k3@users.noreply.github.com>
2022-12-05 14:23:08 +0700

    a

--- app/src/main/java/com/example/duan1/views/OrderHistoryActivity.java
+++ app/src/main/java/com/example/duan1/views/OrderHistoryActivity.java
@@ -5,11 +5,10 @@ import static com.example.duan1.views.HomeScreenActivity.myRef;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -25,7 +24,6 @@ public class OrderHistoryActivity extends AppCompatActivity {
     private Button btnStartOrdering;
     private RecyclerView recyclerViewListHistory;
     HistoryAdapter historyAdapter;
-    private ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,48 +31,31 @@ public class OrderHistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order_history_not_null);
         anhXa();
         setRecyclerViewListHistory();
-//        setSupportActionBar(toolbar);
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("Order History");
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        btnStartOrdering.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(OrderHistoryActivity.this, "This is Button Start Ordering", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-        imgBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Order History");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-        @Override
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-//            getFragmentManager().popBackStack();
             finish();
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void anhXa() {
-//        toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
 //        btnStartOrdering = findViewById(R.id.btnStartOrdering);
         recyclerViewListHistory = findViewById(R.id.recyclerviewListHistory);
-        imgBack = findViewById(R.id.imgBack1);
 
     }
 
-    private void setRecyclerViewListHistory(){
+    private void setRecyclerViewListHistory() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         recyclerViewListHistory.setLayoutManager(linearLayoutManager);
-
-
         FirebaseRecyclerOptions<HoaDon> options =
                 new FirebaseRecyclerOptions.Builder<HoaDon>()
                         .setQuery(myRef().child("HoaDon"), HoaDon.class)
@@ -97,19 +78,6 @@ public class OrderHistoryActivity extends AppCompatActivity {
         intent.putExtras(bundle);
         startActivity(intent);
     }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
     @Override
     protected void onStop() {
         super.onStop();
